52c7746e1943814ee65c0d79d733911d2ec4aecc
AGrigoruta <alexander.grigoruta@gmail.com>
2019-05-25 15:36:34 +0300

    styled the header a bit

--- esox/src/components/Header.jsx
+++ esox/src/components/Header.jsx
@@ -8,8 +8,34 @@ class HeaderComponent extends React.Component {
 
     render() {
         return (
-            <div>
-                Header
+            <div className="header__wrapper">
+                <div className="header__navigation">
+                    <div className="header__branding">
+                        <i className="header__icon fas fa-scroll"></i>
+                        <span className="header__title">EsoX</span>
+                    </div>
+                    <div className="header__tabs">
+                        {this.props.header.tabs.map((item => (
+                            <div className="header__tab">
+                                <span className={this.props.header.selectedTab === item ? 'tab__selected' : ''} key={item}>{item}</span>
+                                {this.props.header.selectedTab === item ? (<div className="tab__underline"></div>) : ''}
+                            </div>
+                        )))}
+                    </div>
+                </div>
+                <div className="header__user">
+                    {this.props.header.loggedIn ? (
+                        <div className="header__user-details">
+                            <img className="header__user-image" src={this.props.header.user.img}></img>
+                            <span className="header__user-name">{this.props.header.user.name}</span>
+                        </div>
+                    ) : (
+                        <button className="esox__button header__login isCarrot">
+                            <i className="fas fa-sign-in-alt"></i>
+                            <span>Login</span>
+                        </button>
+                    )}
+                </div>
             </div>
         );
     }
